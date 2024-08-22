cd67a9141b7b4320fc306be805f10f55f8e1c9ba
Hammad Ali <hammadaali02@gmail.com>
2024-08-23 00:22:30 +0500

    updated

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
+import 'package:nirvana_app/screens/menu_screen.dart';
 import 'package:nirvana_app/utils/app_colors.dart';
 
 class HomeScreen extends StatefulWidget {
@@ -19,14 +20,83 @@ class _HomeScreenState extends State<HomeScreen> {
           child: Column(
             children: [
               Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
                 children: [
                   RichText(
                     text: TextSpan(
                       text: 'Nirvana',
                       style: GoogleFonts.ibmPlexMono(
-                        color: green,
+                        color: purple,
                         fontWeight: FontWeight.bold,
                         fontSize: 24,
+                      ),
+                      children: [
+                        TextSpan(
+                          text: ' Space',
+                          style: GoogleFonts.ibmPlexMono(
+                            color: pink,
+                            fontWeight: FontWeight.bold,
+                            fontSize: 24,
+                          ),
+                        ),
+                        TextSpan(
+                          text: '\nCreative',
+                          style: GoogleFonts.ibmPlexMono(
+                            color: orange,
+                            fontWeight: FontWeight.bold,
+                            fontSize: 24,
+                          ),
+                        ),
+                        TextSpan(
+                          text: ' Hub',
+                          style: GoogleFonts.ibmPlexMono(
+                            color: green,
+                            fontWeight: FontWeight.bold,
+                            fontSize: 24,
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                  GestureDetector(
+                    onTap: () {
+                      Navigator.push(
+                        context,
+                        MaterialPageRoute(
+                          builder: (context) => const MenuScreen(),
+                        ),
+                      );
+                    },
+                    child: Container(
+                      height: 51,
+                      width: 51,
+                      decoration: const BoxDecoration(
+                        color: green,
+                        shape: BoxShape.circle,
+                        boxShadow: [
+                          BoxShadow(
+                            color: pink,
+                            spreadRadius: 0,
+                            blurRadius: 0,
+                            offset: Offset(1.5, 3),
+                          ),
+                          BoxShadow(
+                            color: yellow,
+                            spreadRadius: 0,
+                            blurRadius: 0,
+                            offset: Offset(1.5, 1.5),
+                          ),
+                        ],
+                      ),
+                      child: Center(
+                        child: Text(
+                          'Menu',
+                          style: GoogleFonts.jotiOne(
+                            fontWeight: FontWeight.w300,
+                            fontSize: 12,
+                            color: black,
+                          ),
+                        ),
                       ),
                     ),
                   ),
