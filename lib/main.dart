f85b33c19a4e67416e8b408ebeb9ed622156ee0e
Hammad Ali <hammadaali02@gmail.com>
2024-08-22 23:25:56 +0500

    updated

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:nirvana_app/screens/home_screen.dart';
+import 'package:nirvana_app/screens/splash_screen.dart';
 
 void main() {
   runApp(const MyApp());
@@ -13,7 +13,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return const MaterialApp(
       title: 'Nirvana App',
-      home: HomeScreen(),
+      home: SplashScreen(),
     );
   }
 }
