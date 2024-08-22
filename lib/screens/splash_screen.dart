2d663023ba28deae0c2b7d4e94e3e807cceb975c
Hammad Ali <hammadaali02@gmail.com>
2024-08-22 23:49:30 +0500

    updated

--- lib/screens/splash_screen.dart
+++ lib/screens/splash_screen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
+import 'package:nirvana_app/screens/home_screen.dart';
 import 'package:nirvana_app/utils/app_colors.dart';
 
 class SplashScreen extends StatefulWidget {
@@ -29,31 +30,41 @@ class _SplashScreenState extends State<SplashScreen> {
           const Spacer(),
           Padding(
             padding: const EdgeInsets.symmetric(horizontal: 19.0),
-            child: Container(
-              height: 56,
-              width: double.infinity,
-              decoration: BoxDecoration(
-                color: green,
-                borderRadius: BorderRadius.circular(12),
-                border: Border.all(
-                  color: black,
-                ),
-                boxShadow: const [
-                  BoxShadow(
-                    color: black,
-                    spreadRadius: 0,
-                    blurRadius: 0,
-                    offset: Offset(1.5, 3),
+            child: GestureDetector(
+              onTap: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                    builder: (context) => const HomeScreen(),
                   ),
-                ],
-              ),
-              child: Center(
-                child: Text(
-                  'Get Started',
-                  style: GoogleFonts.ibmPlexMono(
-                    fontWeight: FontWeight.bold,
-                    fontSize: 32,
+                );
+              },
+              child: Container(
+                height: 56,
+                width: double.infinity,
+                decoration: BoxDecoration(
+                  color: green,
+                  borderRadius: BorderRadius.circular(12),
+                  border: Border.all(
                     color: black,
+                  ),
+                  boxShadow: const [
+                    BoxShadow(
+                      color: black,
+                      spreadRadius: 0,
+                      blurRadius: 0,
+                      offset: Offset(1.5, 3),
+                    ),
+                  ],
+                ),
+                child: Center(
+                  child: Text(
+                    'Get Started',
+                    style: GoogleFonts.ibmPlexMono(
+                      fontWeight: FontWeight.bold,
+                      fontSize: 32,
+                      color: black,
+                    ),
                   ),
                 ),
               ),
