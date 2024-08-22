2c3d5e1164c6bc672013f3234aae904e7cd584b0
Hammad Ali <hammadaali02@gmail.com>
2024-08-22 23:00:25 +0500

    colors added

---
+++ lib/utils/app_colors.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  static const Color lightBlue = Color(0xFFADE3FF);
+  static const Color green = Color(0xFF26AF55);
+  static const Color purple = Color(0xFF7F579D);
+  static const Color pink = Color(0xFFFF3C97);
+  static const Color orange = Color(0xFFFA8715);
+  static const Color darkGreen = Color(0xFF21A051);
+  static const Color hotPink = Color(0xFFF259A0);
+  static const Color white = Color(0xFFFFFFFF);
+  static const Color black = Color(0xFF000000);
+  static const Color yellow = Color(0xFFFEED21);
+  static const Color peach = Color(0xFFFFF2E8);
+}
