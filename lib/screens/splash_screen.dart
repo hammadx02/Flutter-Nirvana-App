de756304e4d1bc3a9c88aaed1d12b2f9fa32c354
Hammad Ali <hammadaali02@gmail.com>
2024-08-22 23:41:01 +0500

    splash updated

--- lib/screens/splash_screen.dart
+++ lib/screens/splash_screen.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:nirvana_app/utils/app_colors.dart';
 
 class SplashScreen extends StatefulWidget {
   const SplashScreen({super.key});
@@ -10,6 +12,58 @@ class SplashScreen extends StatefulWidget {
 class _SplashScreenState extends State<SplashScreen> {
   @override
   Widget build(BuildContext context) {
-    return const Scaffold();
+    return Scaffold(
+      backgroundColor: lightBlue,
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: [
+          const Spacer(),
+          SizedBox(
+            height: 350,
+            width: double.infinity,
+            child: Image.asset(
+              'assets/images/logo.png',
+            ),
+          ),
+          const Spacer(),
+          Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 19.0),
+            child: Container(
+              height: 56,
+              width: double.infinity,
+              decoration: BoxDecoration(
+                color: green,
+                borderRadius: BorderRadius.circular(12),
+                border: Border.all(
+                  color: black,
+                ),
+                boxShadow: const [
+                  BoxShadow(
+                    color: black,
+                    spreadRadius: 0,
+                    blurRadius: 0,
+                    offset: Offset(1.5, 3),
+                  ),
+                ],
+              ),
+              child: Center(
+                child: Text(
+                  'Get Started',
+                  style: GoogleFonts.ibmPlexMono(
+                    fontWeight: FontWeight.bold,
+                    fontSize: 32,
+                    color: black,
+                  ),
+                ),
+              ),
+            ),
+          ),
+          const SizedBox(
+            height: 40,
+          ),
+        ],
+      ),
+    );
   }
 }
--- lib/utils/app_colors.dart
+++ lib/utils/app_colors.dart
@@ -1,15 +1,13 @@
 import 'package:flutter/material.dart';
 
-class AppColors {
-  static const Color lightBlue = Color(0xFFADE3FF);
-  static const Color green = Color(0xFF26AF55);
-  static const Color purple = Color(0xFF7F579D);
-  static const Color pink = Color(0xFFFF3C97);
-  static const Color orange = Color(0xFFFA8715);
-  static const Color darkGreen = Color(0xFF21A051);
-  static const Color hotPink = Color(0xFFF259A0);
-  static const Color white = Color(0xFFFFFFFF);
-  static const Color black = Color(0xFF000000);
-  static const Color yellow = Color(0xFFFEED21);
-  static const Color peach = Color(0xFFFFF2E8);
-}
+const Color lightBlue = Color(0xFFADE3FF);
+const Color green = Color(0xFF26AF55);
+const Color purple = Color(0xFF7F579D);
+const Color pink = Color(0xFFFF3C97);
+const Color orange = Color(0xFFFA8715);
+const Color darkGreen = Color(0xFF21A051);
+const Color hotPink = Color(0xFFF259A0);
+const Color white = Color(0xFFFFFFFF);
+const Color black = Color(0xFF000000);
+const Color yellow = Color(0xFFFEED21);
+const Color peach = Color(0xFFFFF2E8);
