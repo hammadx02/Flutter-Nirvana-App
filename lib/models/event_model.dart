84247df7f99de88118fd14153dbbe2b0cb7922fe
Hammad Ali <hammadaali02@gmail.com>
2024-08-23 14:37:52 +0500

    model added

---
+++ lib/models/event_model.dart
@@ -0,0 +1,15 @@
+class EventModel {
+  final String image;
+  final String title;
+  final String time;
+  final String date;
+  final String location;
+
+  EventModel({
+    required this.image,
+    required this.title,
+    required this.time,
+    required this.date,
+    required this.location,
+  });
+}
